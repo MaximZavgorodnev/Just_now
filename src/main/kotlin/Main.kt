5e9343b77cd3352b2c4f7dd376a7b296da73435e
MaximZavgorodnev <maxpek9@yandex.ru>
2021-10-22 18:56:46 +0300

    Исправил correctMinutes, agoToText, correctHours

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,54 +4,44 @@ import kotlin.math.roundToInt
 const val HOUR = 60*60
 
 fun main(){
-    val inputTime = 61 //Входные данные в секундах
+    val inputTime = 600 //Входные данные в секундах
 
     val inputText = agoToText(inputTime)
     println("В сети был(а) $inputText")
 }
 
 fun agoToText(inputTime: Int): String {
-    val callPeriod: String
-    when (inputTime) {
-        in 0..60 -> callPeriod = "только что" //только что
-        in 61..HOUR -> callPeriod = correctMinutes(inputTime) // Х минут назад
-        in HOUR+1..24*HOUR -> callPeriod = correctHours(inputTime) // Х часов назад
-        in 24*HOUR..2*24*HOUR -> callPeriod = "сегодня" // сегодня
-        in 2*24*HOUR..3*24*HOUR -> callPeriod = "вчера" // вчера
-        in 3*24*HOUR..Int.MAX_VALUE -> callPeriod = "давно" // давно
-        else -> callPeriod = "никогда не была"
+
+    val callPeriod = when (inputTime) {
+        in 0..60 -> "только что" //только что
+        in 61..HOUR ->  correctMinutes(inputTime) // Х минут назад
+        in HOUR+1..24*HOUR -> correctHours(inputTime) // Х часов назад
+        in 24*HOUR..2*24*HOUR -> "сегодня" // сегодня
+        in 2*24*HOUR..3*24*HOUR -> "вчера" // вчера
+        in 3*24*HOUR..Int.MAX_VALUE -> "давно" // давно
+        else -> "никогда не была"
     }
     return callPeriod
 }
 
 fun correctMinutes(inputTime: Int): String {
-    val minutes: String
-    var m = (inputTime / 60.0).roundToInt()
-    when(m){
-        1,21,31,41,51 -> minutes = "$m минуту назад"
-        in 2..4 -> minutes = "$m минуты назад"
-        in 5..20, -> minutes = "$m минут назад"
-        in 22..24, -> minutes = "$m минуты назад"
-        in 25..30, -> minutes = "$m минут назад"
-        in 32..34, -> minutes = "$m минуты назад"
-        in 35..40, -> minutes = "$m минут назад"
-        in 42..44, -> minutes = "$m минуты назад"
-        in 45..50, -> minutes = "$m минут назад"
-        in 52..54, -> minutes = "$m минуты назад"
-        in 55..60, -> minutes = "$m минут назад"
-        else -> minutes = "$m минут назад"
+    val m = (inputTime / 60.0).roundToInt()
+    val minutes = when(m%10){
+        1 -> "$m минуту назад"
+        in 2..4 -> "$m минуты назад"
+        in 5..9, -> "$m минут назад"
+        else -> "$m минут назад"
     }
     return minutes
 }
 
 fun correctHours(inputTime: Int): String {
-    val hours: String
-    var h = (inputTime / HOUR)
-    when (h){
-        1,21 -> hours = "$h час назад"
-        in 2..4 -> hours = "$h часа назад"
-        in 5..20 -> hours = "$h часов назад"
-        else -> hours ="$h часа назад"
+    val h = (inputTime / HOUR)
+    val hours = when (h){
+        1,21 -> "$h час назад"
+        in 2..4 -> "$h часа назад"
+        in 5..20 -> "$h часов назад"
+        else -> "$h часа назад"
     }
     return hours
 }
